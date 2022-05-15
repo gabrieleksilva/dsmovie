a022ce448605903422ab0213b39ddf9d2c294714
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2022-05-15 01:56:49 -0300

    Show score

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -23,7 +23,7 @@ function Listing() {
 
     //FORMA ERRADA
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
             .then(response => {
                 const data = response.data as MoviePage;
                 setPage(data);
