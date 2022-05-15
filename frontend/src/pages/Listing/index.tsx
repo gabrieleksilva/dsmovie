8be930386124a55603e7732af24a555352ceba33
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2022-05-15 10:36:24 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -21,7 +21,7 @@ function Listing() {
         empty: true
     });
 
-    //FORMA ERRADA
+    
     useEffect(() => {
         axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
             .then(response => {
@@ -31,10 +31,13 @@ function Listing() {
 
     }, [pageNumber]);
 
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange ={handlePageChange}/>
 
             <div className="container">
                 <div className="row">
