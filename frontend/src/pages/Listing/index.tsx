c870aa085e6ec5253b39e39cda28e114b5fdd5bf
Gabriele K Silva <gabriele.s@aluno.ifsp.edu.br>
2022-05-15 00:16:52 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,19 +1,30 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "utils/request";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
+    const [pageNumber, setPageNumber] = useState(0);
+
     //FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=0'`)
-    .then(response => {
-        console.log(response.data);
-    })
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        .then(response => {
+             const data = response.data as MoviePage;
+             console.log(data);
+             setPageNumber(data.number);
+        });
+
+    }, []);
+    
 
 
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
